a405fd8972df4a8c8e5f0d30b7f1e397aa218ac2
Andrew Procter <acprocter@gmail.com>
2017-02-15 16:42:41 -0500

    update

--- PCA - leukemia gene expression.R
+++ PCA - leukemia gene expression.R
@@ -1,12 +1,17 @@
 # Linear algebra HW - gene expression for Leukemia
 
+# Notes:
+# Dataset contains gene expression profiles for three types of Leukemia.
+# Due to a lab error, there is a chance that one of the samples is mislabeled.
+# Assignment is to determine which of the samples that may be.
+
 load('C:/Users/Andrew/Documents/NC State Center for Advanced Analytics/Linear Algebra/HW/LeukError.Rdata')
 
-names(leuk)
-names(B)
+names(leuk) #5000 genes
+length(leuk)
 
-leuk$V5001 # this says what type of leukemia it is
-            # ALL-B, ALL-T, AML
+leuk$V5001 # this column says what type of leukemia it is
+           # ALL-B, ALL-T, AML
            # The data has 38 rows = 38 labeled samples
 
 # Compute covariance PCA (note the scale=F option)
@@ -19,9 +24,9 @@ head(pca)
 
 summary(pca)
 
-plot(pca$x)
+plot(pca$x) #plot the first and second principal components
 
-# Pick out a color palatte for the Leukemia type that would make Dr. Healey Proud:
+# Pick out a color palatte for the Leukemia type:
 
 colors <- c("cyan",
             "red",
@@ -37,28 +42,15 @@ text(pca$x[,1], pca$x[,2],leuk$V5001, cex=0.5)
 
 library(rgl)
 library(car)
-scatter3d(x=pca$x[,1],y=pca$x[,2],z=pca$x[,3], groups=leuk$V5001, surface=F, labels=leuk$V5001, id.n = 38) #id.n looks like the number of observations
+scatter3d(x=pca$x[,1],y=pca$x[,2],z=pca$x[,3], groups=leuk$V5001, surface=F, labels=leuk$V5001, id.n = 38) #id.n is the number of observations
 
 # based on the 2D and 3D plots, it looks like the AML sample
 # at less than -20000 on PC1 may actually be ALL-T
 
 pc1 = cbind(pca$x[,1], leuk$V5001) # ALL-B = 1, ALL-T = 2, AML = 3
 
-AML = pc1[pc1[,2]==3,]
+AML = pc1[pc1[,2]==3,] #identify the PC1 scores for samples labeled AML
 
 # Sample 22 is probably mislabeled and should be ALL-T
 # It already looks suspicious because in the column of
 # leukemia types it is a 3 in the middle of 2's
-
-
-#  Code from other PCA examples ######################################
-
-# Get the dataset in order, create the target variable which is missing:
-
-chicken = as.data.frame(t(chicken))
-diet = as.factor(c(rep("N",6),rep("F16",5),rep("F16R5",8), rep("F16R16",9),rep("F48",6),rep("F48R24",9)))
-chicken = cbind.data.frame(diet,chicken)
-
-# Compute covariance PCA (note the scale=F option)
-
-pca = prcomp(chicken[,2:7407], scale=F) # Do not include the factor diet in the PCA computation
